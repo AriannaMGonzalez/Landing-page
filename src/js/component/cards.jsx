4842b4a6d988c25a0220b01c05d720300b93fe5b
AriannaMGonzalez <Jebuschan@live.com>
2022-10-31 16:55:26 +0000

    Initial Commit

---
+++ src/js/component/cards.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Card from './card.jsx';
+
+const Cards = () => {
+    return (
+        <div className="container-flex">
+            <div className="row">
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </div>
+        </div>
+    );
+};
+
+export default Cards;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,23 +3,31 @@ import React from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
+// importing components
+import Navbar from "./navbar.jsx";
+import Jumbotron from "./jumbotron.jsx";
+import Cards from "./cards.jsx";
+import Footer from './footer.jsx';
+
+const links = [
+	{linkText: 'Home', url: '#'},
+	{linkText: 'About', url: '#'},
+	{linkText: 'Services', url: '#'},
+	{linkText: 'Contact', url: '#'},
+];
+
 //create your first component
 const Home = () => {
+	
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div  className="mx-3">
+				<Navbar links={links}/>
+				<Jumbotron />
+				<Cards />
+			</div>
+			<Footer />
+		</>
 	);
 };
 
